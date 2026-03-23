e24473ba05138d9769b90d13a112b9f7f8e0715b
Jay <jaydeore.work@gmail.com>
2026-03-23 09:00:03 +0000

    Refactor: Implement functional production-ready pipeline and models

--- discriminator.py
+++ discriminator.py
@@ -1,3 +1,20 @@
 import torch.nn as nn
+import numpy as np
+
 class Discriminator(nn.Module):
-    pass
+    def __init__(self, img_shape=(1, 28, 28)):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(img_shape)), 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 128),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(128, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, img):
+        img_flat = img.view(img.size(0), -1)
+        validity = self.model(img_flat)
+        return validity
